268d280cff5d7ef45fe310e32c027124fe91d950
Jonas Kaiser <jonas.kaiser@kabelmail.de>
2019-03-13 16:06:31 +0100

    moved config.def.h changes to config.h

--- config.h
+++ config.h
@@ -17,19 +17,27 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_black[]       = "#000000";
 static const char col_green[]       = "#273234"; // darkgreen
-static const char col_red[]         = "#770000";
+//static const char col_red[]         = "#770000";
+static const char col_red[]         = "#ff0000";
+static const char col_yellow[]      = "#ffff00";
+static const char col_white[]       = "#ffffff";
 static const unsigned int baralpha = 0xd1;      // should be about 80% (to look like xdefaults)
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_green,  col_red   },
+	[SchemeSel]  = { col_gray4, col_green, col_red   },
+	[SchemeWarn]   = { col_black, col_yellow, col_red   },
+	[SchemeUrgent] = { col_white, col_red,    col_red   },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+	[SchemeWarn] = { OPAQUE, baralpha, borderalpha },
+	[SchemeUrgent]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
