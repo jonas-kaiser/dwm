2f6fa72498fa804b487936f9c9e2e10edd20f76d
Jonas Kaiser <jonas.kaiser@kabelmail.de>
2019-08-27 21:04:17 +0200

    Added cfacts to centeredmaster layout

--- centeredmaster.c
+++ centeredmaster.c
@@ -2,12 +2,25 @@ void
 centeredmaster(Monitor *m)
 {
 	unsigned int i, n, h, mw, mx, my, oty, ety, tw;
+	float mfacts = 0, lfacts = 0, rfacts = 0;
 	Client *c;
 
 	/* count number of clients in the selected monitor */
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++) {
+		if (n < m->nmaster)
+			mfacts += c->cfact;
+		else if ((n - m->nmaster) % 2)
+			lfacts += c->cfact;
+		else
+			rfacts += c->cfact;
+	}
 	if (n == 0)
 		return;
+	if (n == 1) {
+		c = nexttiled(m->clients);
+		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+		return;
+	}
 
 	/* initialize areas */
 	mw = m->ww;
@@ -33,22 +46,30 @@ centeredmaster(Monitor *m)
 	if (i < m->nmaster) {
 		/* nmaster clients are stacked vertically, in the center
 		 * of the screen */
-		h = (m->wh - my) / (MIN(n, m->nmaster) - i);
+		h = (m->wh - my) * (c->cfact / mfacts);
 		resize(c, m->wx + mx, m->wy + my, mw - (2*c->bw),
 		       h - (2*c->bw), 0);
 		my += HEIGHT(c);
+		mfacts -= c->cfact;
 	} else {
 		/* stack clients are stacked vertically */
-		if ((i - m->nmaster) % 2 ) {
-			h = (m->wh - ety) / ( (1 + n - i) / 2);
-			resize(c, m->wx, m->wy + ety, tw - (2*c->bw),
-			       h - (2*c->bw), 0);
+		if ((i - m->nmaster) % 2) {
+			h = (m->wh - ety) * (c->cfact / lfacts);
+			//TODO don't need if
+			if(m->nmaster == 0)
+				resize(c, m->wx, m->wy + ety, tw - 2*c->bw,
+				       h - (2*c->bw), 0);
+			else
+				resize(c, m->wx, m->wy + ety, tw - 2*c->bw,
+				       h - (2*c->bw), 0);
 			ety += HEIGHT(c);
+			lfacts -= c->cfact;
 		} else {
-			h = (m->wh - oty) / ((1 + n - i) / 2);
+			h = (m->wh - oty) * (c->cfact / rfacts);
 			resize(c, m->wx + mx + mw, m->wy + oty,
 			       tw - (2*c->bw), h - (2*c->bw), 0);
 			oty += HEIGHT(c);
+			rfacts -= c->cfact;
 		}
 	}
 }
