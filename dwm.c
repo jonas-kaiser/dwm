f81d7ada9aea1083873dd43be1870c190eb20989
Jonas Kaiser <jonas.kaiser@kabelmail.de>
2020-01-06 14:56:54 +0100

    manually applied dwm-fakefullscreen-20170508-ceac8c9.diff

--- dwm.c
+++ dwm.c
@@ -673,7 +673,9 @@ clientmessage(XEvent *e)
 		if (cme->data.l[1] == netatom[NetWMFullscreen]
 		|| cme->data.l[2] == netatom[NetWMFullscreen])
 			setfullscreen(c, (cme->data.l[0] == 1 /* _NET_WM_STATE_ADD    */
-				|| (cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */ && !c->isfullscreen)));
+// fakefullscreen patch
+//				|| (cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */ && !c->isfullscreen)));
+				|| (cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */));
 	} else if (cme->message_type == netatom[NetActiveWindow]) {
 		if (c != selmon->sel && !c->isurgent)
 			seturgent(c, 1);
@@ -703,7 +705,8 @@ void
 configurenotify(XEvent *e)
 {
 	Monitor *m;
-	Client *c;
+// fakefullscreen patch
+//	Client *c;
 	XConfigureEvent *ev = &e->xconfigure;
 	int dirty;
 
@@ -715,9 +718,10 @@ configurenotify(XEvent *e)
 		if (updategeom() || dirty) {
 			drw_resize(drw, sw, bh);
 			updatebars();
-			for (m = mons; m; m = m->next) {
-				for (c = m->clients; c; c = c->next)
-					if (c->isfullscreen)
+// fakefullscreen patch
+//			for (m = mons; m; m = m->next) {
+//				for (c = m->clients; c; c = c->next)
+//					if (c->isfullscreen)
 						resizeclient(c, m->mx, m->my, m->mw, m->mh);
 				resizebarwin(m);
 			}
@@ -1352,8 +1356,9 @@ movemouse(const Arg *arg)
 
 	if (!(c = selmon->sel))
 		return;
-	if (c->isfullscreen) /* no support moving fullscreen windows by mouse */
-		return;
+// fakefullscreen patch
+//	if (c->isfullscreen) /* no support moving fullscreen windows by mouse */
+//		return;
 	restack(selmon);
 	ocx = c->x;
 	ocy = c->y;
@@ -1556,8 +1561,9 @@ resizemouse(const Arg *arg)
 
 	if (!(c = selmon->sel))
 		return;
-	if (c->isfullscreen) /* no support resizing fullscreen windows by mouse */
-		return;
+// fakefullscreen patch
+//	if (c->isfullscreen) /* no support resizing fullscreen windows by mouse */
+//		return;
 	restack(selmon);
 	ocx = c->x;
 	ocy = c->y;
@@ -1757,24 +1763,26 @@ setfullscreen(Client *c, int fullscreen)
 		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
 			PropModeReplace, (unsigned char*)&netatom[NetWMFullscreen], 1);
 		c->isfullscreen = 1;
-		c->oldstate = c->isfloating;
-		c->oldbw = c->bw;
-		c->bw = 0;
-		c->isfloating = 1;
-		resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
-		XRaiseWindow(dpy, c->win);
+// fakefullscreen patch
+//		c->oldstate = c->isfloating;
+//		c->oldbw = c->bw;
+//		c->bw = 0;
+//		c->isfloating = 1;
+//		resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
+//		XRaiseWindow(dpy, c->win);
 	} else if (!fullscreen && c->isfullscreen){
 		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
 			PropModeReplace, (unsigned char*)0, 0);
 		c->isfullscreen = 0;
-		c->isfloating = c->oldstate;
-		c->bw = c->oldbw;
-		c->x = c->oldx;
-		c->y = c->oldy;
-		c->w = c->oldw;
-		c->h = c->oldh;
-		resizeclient(c, c->x, c->y, c->w, c->h);
-		arrange(c->mon);
+// fakefullscreen patch
+//		c->isfloating = c->oldstate;
+//		c->bw = c->oldbw;
+//		c->x = c->oldx;
+//		c->y = c->oldy;
+//		c->w = c->oldw;
+//		c->h = c->oldh;
+//		resizeclient(c, c->x, c->y, c->w, c->h);
+//		arrange(c->mon);
 	}
 }
 
@@ -1926,7 +1934,9 @@ showhide(Client *c)
 	if (ISVISIBLE(c)) {
 		/* show clients top down */
 		XMoveWindow(dpy, c->win, c->x, c->y);
-		if ((!c->mon->lt[c->mon->sellt]->arrange || c->isfloating) && !c->isfullscreen)
+// fakefullscreen patch
+//		if ((!c->mon->lt[c->mon->sellt]->arrange || c->isfloating) && !c->isfullscreen)
+		if (!c->mon->lt[c->mon->sellt]->arrange || c->isfloating)
 			resize(c, c->x, c->y, c->w, c->h, 0);
 		showhide(c->snext);
 	} else {
@@ -2039,8 +2049,9 @@ togglefloating(const Arg *arg)
 {
 	if (!selmon->sel)
 		return;
-	if (selmon->sel->isfullscreen) /* no support for fullscreen windows */
-		return;
+// fakefullscreen patch
+//	if (selmon->sel->isfullscreen) /* no support for fullscreen windows */
+//		return;
 	selmon->sel->isfloating = !selmon->sel->isfloating || selmon->sel->isfixed;
 	if (selmon->sel->isfloating)
 		resize(selmon->sel, selmon->sel->x, selmon->sel->y,
